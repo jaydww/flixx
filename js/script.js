e7d054bde5cf8cb8cc90ca4374de80c02ad14295
jaydww <jamesdwalsh@hotmail.com>
2023-06-04 15:03:02 -0600

    display popular shows

--- js/script.js
+++ js/script.js
@@ -36,6 +36,40 @@ async function displayPopularMovies() {
 	});
 }
 
+async function displayPopularShows() {
+	const { results } = await fetchAPIData('tv/popular');
+
+	results.forEach((show) => {
+		const div = document.createElement('div');
+		div.classList.add('card');
+		div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+            ${
+							show.poster_path
+								? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+								: `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+						}
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air date: ${show.first_air_date}</small>
+            </p>
+          </div>
+    `;
+
+		document.querySelector('#popular-shows').appendChild(div);
+	});
+}
+
 // Display movie details
 async function displayMovieDetails() {
 	const movieId = window.location.search.split('=')[1];
@@ -204,13 +238,24 @@ async function fetchAPIData(endpoint) {
 	const API_KEY = '0fd14fe4293bf3ae28b0ed929f6b0ecc';
 	const API_URL = 'https://api.themoviedb.org/3/';
 
+	showSpinner();
+
 	const response = await fetch(
 		`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
 	);
 
 	const data = await response.json();
 
+	hideSpinner();
+
 	return data;
+}
+
+function showSpinner() {
+	document.querySelector('.spinner').classList.add('show');
+}
+function hideSpinner() {
+	document.querySelector('.spinner').classList.remove('show');
 }
 
 // Highlight active link
@@ -235,7 +280,7 @@ function init() {
 			displayPopularMovies();
 			break;
 		case '/shows.html':
-			console.log('Shows');
+			displayPopularShows();
 			break;
 		case '/movie-details.html':
 			displayMovieDetails();
